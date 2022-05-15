42758bd8c48e691d28d7b4939876629432cf6fd8
andydevs69420 <redondophilippandrew@gmail.com>
2022-05-16 00:36:34 +0800

    semi finished

--- abstractPriceQuotationGenerator.js.js
+++ abstractPriceQuotationGenerator.js.js
@@ -3,7 +3,7 @@
     | by andydevs69420
     | github: https://github.com/andydevs69420/dict-abstractPriceQuotationGenerator.git
     | Abstract price quotation generator is a simple tool that generates price quotations for DICT.
-    | May 11 2022
+    | May 14 2022
     ;
 */
 
@@ -156,12 +156,21 @@
             this.PURPOSE_SLOT_ID = "purpose-slot-value";
 
             this.PARENT      = document.getElementsByTagName(this.LOOKUP_TAG)[instance_no];
+            setStyle(this.PARENT, {
+                "display"     : "flex",
+                "align-items" : "center",
+                "justify-content": "center",
+            });
             this.SIZE        = verifySize(option.size);
             this.ORIENTATION = option.orientation;
 
-            this.SUPPLIERS  = null;
-            this.PURPOSE    = null;
-            this.PAGE_HEAD  = [];
+            this.PURPOSE     = null;
+            this.SUPPLIERS   = null;
+            this.HEADERLST   = null;
+            this.PAGE_HEAD   = [];
+            this.COLSPANS    = [];
+            this.ITEM_LIST   = [];
+            this.ITEM_ROWS   = [];
 
         }
 
@@ -178,7 +187,7 @@
                     "margin" : "auto" ,
                     "width"  : this.SIZE._w + "mm",
                     "height" : this.SIZE._h + "mm",
-                    "background-color" : "#FFFFFF"
+                    "background-color" : "#FFFFFF",
                 });
             
             if (this.ORIENTATION === PAPER_ORIENTATION.LANDSCAPE) {
@@ -196,13 +205,18 @@
          * @return HTMLElement
          * @example
          **/
-        make__Table(header, hasPurpose=true) {
+        make__Table(wrapper, header, hasPurpose=true) {
+
+            if (!wrapper instanceof HTMLElement)
+            throw new TypeError("wrapper must be an HTMLElement type!, got "+wrapper.constructor.name);
 
             if (!header.constructor.name === "Array")
             throw new TypeError("header must be an Array type!, got "+header.constructor.name);
 
             if (!hasPurpose.constructor.name === "Boolean")
             throw new TypeError("hasPurpose must be an Boolean type!, got "+hasPurpose.constructor.name);
+
+            let table_or_paper = [];
 
             let table = $el("table", {
                 style : {
@@ -248,7 +262,7 @@
                             }
                         });
 
-                        td_purpose_slot.innerText = this.PURPOSE;
+                        td_purpose_slot.innerText = this.truncate__Text(this.PURPOSE, 255);
 
                     tr_purpose_group.appendChild(th_purpose_label);
                     tr_purpose_group.appendChild(td_purpose_slot);
@@ -264,6 +278,16 @@
                         }
                     });
 
+                    // sort header
+                    header.sort((a, b) => {
+                        let h_0 = this.HEADERLST.map((arr) => arr[0]);
+                        let h_1 = this.HEADERLST.map((arr) => arr[1]);
+                        if (h_0[h_1.indexOf(a)] > h_0[h_1.indexOf(b)]) return 1;
+                        if (h_0[h_1.indexOf(a)] < h_0[h_1.indexOf(b)]) return -1;
+                        return 0;
+                  
+                    });
+
                     let max_idx = 0;
 
                     for (let iter_i = 0; iter_i < header.length; iter_i++)
@@ -272,7 +296,27 @@
                                 max_idx = iter_j;
 
                     let iter_idx = 0;
+                    let colspan = [];
                     header.forEach((th_val) => {
+
+                        colspan.push((iter_idx !== max_idx)? (() => {
+                            let colspan = this.NUMBER_OF_UNITS - (this.NUMBER_OF_UNITS) - (header.length - 1);
+
+                            if (colspan <= 0)
+                                return "1";
+
+                            return colspan;
+                        })()
+                        : 
+                        (() => {
+                            let colspan = (this.NUMBER_OF_UNITS) - (header.length - 1);
+
+                            if (colspan <= 0)
+                                return "1";
+
+                            return colspan;
+                        })());
+
 
                         let th_as_column = $el("th", {
                             style  : {
@@ -282,39 +326,79 @@
                                 "overflow-wrap" : "break-word",
                             },
                             attrib : {
-                                "colspan" : (iter_idx !== max_idx)? (() => {
-                                    let colspan = this.NUMBER_OF_UNITS - (this.NUMBER_OF_UNITS) - (header.length - 1);
-
-                                    if (colspan <= 0)
-                                        return "1";
-
-                                    return colspan;
-                                })()
-                                : 
-                                (() => {
-                                    let colspan = (this.NUMBER_OF_UNITS) - (header.length - 1);
-
-                                    if (colspan <= 0)
-                                        return "1";
-
-                                    return colspan;
-                                })()
+                                "colspan" : colspan[colspan.length - 1]
                             }
                         });
 
-                        th_as_column.innerText = th_val;
+                        th_as_column.innerText = this.truncate__Text(th_val, 40);
 
                         tr_header_group.appendChild(th_as_column);
 
                         iter_idx++;
                     });
+
+                    this.COLSPANS.push(colspan);
                 
                 thead_1.appendChild(tr_header_group);
-
+            
+        
             table.appendChild(thead_0);
             table.appendChild(thead_1);
-
+                
             return table;
+        }
+
+        append__Rows(table, page_num_in_index) {
+
+            if (!table instanceof HTMLElement)
+            throw new TypeError("table must be an HTMLElement type!, got "+table.constructor.name);
+
+            if (page_num_in_index.constructor.name !== "Number")
+            throw new TypeError("page_num_in_index must be an Number type!, got "+page_num_in_index.constructor.name);
+
+            this.ITEM_ROWS[page_num_in_index].forEach((row) => {
+                let tr_row = $el("tr", {
+                    style : {
+                        "border-bottom" : "1px solid black",
+                    }
+                });
+                
+                let iter_idx = 0;
+                row.forEach((td_val) => {
+                    let td_as_column = $el("td", {
+                        style  : {
+                            "padding"       : ".8em .2em",
+                            "text-align"    : "center",
+                            "border-left"   : (iter_idx !== 0) ? "1px solid black" : "none",
+                            "overflow-wrap" : "break-word",
+                        },
+                        attrib : {
+                            "colspan" : this.COLSPANS[page_num_in_index][iter_idx]
+                        }
+                    });
+
+                    td_as_column.innerText = this.truncate__Text(td_val.toString(), 40);
+
+                    tr_row.appendChild(td_as_column);
+                    iter_idx++;
+                });
+                
+                table.appendChild(tr_row);
+            });
+
+        }
+
+        truncate__Text(text, limit=50) {
+
+            if (text.constructor.name !== "String")
+            throw new TypeError("text must be a String type!, got "+text.constructor.name);
+
+            if (limit.constructor.name !== "Number")
+            throw new TypeError("limit must be a Number type!, got "+limit.constructor.name);
+
+            if (text.length > (limit - 3))
+                return text.substring(0, (limit - 3)) + "...";
+            return text;
         }
 
         // #=================================== SETTERS =================================
@@ -324,13 +408,19 @@
 
             if (string.constructor.name !== "String")
             throw new TypeError("string must be an String type!, got "+string.constructor.name);
+
             this.PURPOSE = string;
+
         }
 
         markAsSupplier(suppliers) {
 
+            if (!this.PURPOSE)
+            throw new Error("Purpose must be set before marking as supplier!");
+
             if (suppliers.constructor.name !== "Array")
             throw new TypeError("suppliers must be an Array type!, got "+suppliers.constructor.name);
+
             this.SUPPLIERS = suppliers;
 
         }
@@ -338,7 +428,7 @@
         setColumnHeader(header) {
 
             if (!this.SUPPLIERS)
-            throw new Error("Suppliers not set!");
+            throw new Error("Suppliers must be set before setting column header!");
 
             if (header.constructor.name !== "Array")
             throw new TypeError("header must be an Array type!, got "+header.constructor.name);
@@ -348,47 +438,127 @@
                 throw new Error("supplier "+supplier+" is not found in the given column header!");
             });
 
-            let header_copy = header;
+            this.HEADERLST = header;
 
             while (this.COL_HEADER?.firstChild)
             this.COL_HEADER?.removeChild(this.COL_HEADER.firstChild);
 
-            if (this.NUMBER_OF_UNITS < header.length)
-                while (this.SUPPLIERS.length > 0)
-                    this.PAGE_HEAD.push(this.SUPPLIERS.splice(0, 3));
+            let supplier_copy = this.SUPPLIERS.map((supp) => supp);
 
-            console.log(header_copy);
+            let required_head = header.map((h) => h[1]).filter((e) => !supplier_copy.includes(e));
+
+            while (supplier_copy.length > 0) {
+                let filtered_header = required_head.map((item) => item);
+                filtered_header = filtered_header.concat(supplier_copy.splice(0, 3));
+                this.PAGE_HEAD.push(filtered_header);
+            }
+
+        }
+
+        addRow(row) {
+
+            if (row.constructor.name !== "Array")
+            throw new TypeError("row must be an Array type!, got "+row.constructor.name);
             
+            if (row.length !== this.HEADERLST.length)
+            throw new Error("invalid row column count! expected "+this.HEADERLST.length+", got "+row.length);
+
+            this.ITEM_LIST.push(row);
+            this.ITEM_ROWS = [];
+
+            this.PAGE_HEAD.forEach((header) => {
+
+                let page_supplier_indexes = this.SUPPLIERS.filter((e) => {
+                    return header.includes(e);
+                }).map((s) => this.HEADERLST.map((h) => h[1]).indexOf(s));
+
+                console.log(page_supplier_indexes);
+
+                let supplier_copy = this.SUPPLIERS.map((supp) => supp);
+                let required_head_indexes = this.HEADERLST.map((h) => h[1]).filter((e) => !supplier_copy.includes(e)).map((rh) => this.HEADERLST.map((hl) => hl[1]).indexOf(rh));
+
+                let row_indexes = (required_head_indexes.concat(page_supplier_indexes)).sort((a, b) => a < b ? -1 : 1);
+                
+                let current_page = [];
+                
+                this.ITEM_LIST.forEach((item_row) => {
+
+                    let current_row = [];
+
+                    row_indexes.forEach((idx) => {
+                        current_row.push(item_row[idx]);
+                    });
+
+                    current_page.push(current_row);
+
+                });
+
+                this.ITEM_ROWS.push(current_page);
+                
+            });
+
         }
 
         generatexxxx() {
 
-            let hasPurpose = true;
+            if (!this.HEADERLST)
+            throw new Error("Column header must be set before generating!");
 
-            this.PAGE_HEAD.forEach((h) => {
+            let PAPER__WRAPPER = $el("div", {
+                    style : {
+                        "width" : "fit-content",
+                    }
+            })
+
+            this.PARENT?.appendChild(PAPER__WRAPPER);
+
+            if (this.PAGE_HEAD.length === 1) {
 
                 let paper = this.make__Paper();
-                    let table = this.make__Table(h, hasPurpose);
+                    let wrapper = $el("div");
+                        let table = this.make__Table(wrapper, this.PAGE_HEAD[0], true);
 
-                paper.appendChild(table);
+                paper.appendChild(wrapper);
+                PAPER__WRAPPER?.appendChild(paper);
 
-                hasPurpose = false;
-                this.PARENT?.append(paper);
+            } else {
 
-            });
+                let idx = 0;
+                let hasPurpose = true;
 
+                this.PAGE_HEAD.forEach((h) => {
 
-            let size = [this.PAPER.offsetWidth, this.PAPER.offsetHeight];
+                    let paper = this.make__Paper();
+                        let wrapper = $el("div");
+                            let table = this.make__Table(wrapper, h, hasPurpose);
+                            this.append__Rows(table, idx);
+                        wrapper.appendChild(table);
+                    idx++;
+                    hasPurpose = false;
+                    paper.appendChild(wrapper);
+                    PAPER__WRAPPER?.appendChild(paper);
+                    
+                });
 
-            let doc = new jspdf.jsPDF({
+            }
+
+            let size = [
+                (this.SIZE._w * 3.779527559),
+                (this.SIZE._h * 3.779527559),
+            ];
+
+            let docx = new jspdf.jsPDF({
                 orientation : this.ORIENTATION,
                 format      : (this.ORIENTATION === "l")? size.reverse() : size,
                 unit        : "px",
             });
-            
-            doc.html(this.PAPER, {
+
+            docx.html(PAPER__WRAPPER, {
+                x: 0,
+                y: 0,
                 callback: function (doc) {
-                    doc.output("dataurlnewwindow");
+                    // doc.output("dataurlnewwindow");
+                    // doc.save("test.pdf");
                 }
             });
         }
